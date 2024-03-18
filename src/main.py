1d2616de714c2541f12de74e8ac136ba1938dcaf
Pranav Kulkarni <pranavkop@live.com>
2024-03-18 12:46:19 -0400

    Added results, figures. Updated docs.

--- src/main.py
+++ src/main.py
@@ -8,25 +8,20 @@ from analysis import *
 parser = argparse.ArgumentParser()
 # parser.add_argument('-train_test_aim_1_pnm', action='store_true')
 # parser.add_argument('-train_test_aim_1_ptx', action='store_true')
-# parser.add_argument('-train_test_hulk0', action='store_true')
-# parser.add_argument('-train_test_hulk1', action='store_true')
-# parser.add_argument('-train_test_hulk2', action='store_true')
-# parser.add_argument('-train_test_hulk3', action='store_true')
-# parser.add_argument('-train_test_hulk4', action='store_true')
-# parser.add_argument('-train_test_hulk5', action='store_true')
-# parser.add_argument('-train_test_hulk6', action='store_true')
-# parser.add_argument('-train_test_hulk7', action='store_true')
+parser.add_argument('-train', action='store_true')
+parser.add_argument('-train_baseline', action='store_true')
 parser.add_argument('-model', default='densenet', choices=['densenet', 'resnet', 'inception'])
 parser.add_argument('-test_ds', default='rsna', choices=['rsna', 'mimic', 'cxpt'])
 parser.add_argument('-test', action='store_true')
 parser.add_argument('-analyze', action='store_true')
+
 args = parser.parse_args()
 model = args.model
 test_ds = args.test_ds
 
 def train_test_aim_2(sex=None, age=None):
   train_aim_2(model, sex, age)
-  test_aim_2(model, sex, age)
+  test_aim_2(model, test_ds, sex, age)
 
 if __name__=='__main__':
   gpus = tf.config.list_physical_devices('GPU')
@@ -50,91 +45,47 @@ if __name__=='__main__':
   #   train_aim_1_ptx()
   #   test_aim_1_ptx()
     
-  # if args.train_test_pranav:
-  #   train_aim_2(sex='M')
-  #   train_aim_2(sex='F')
-  #   train_aim_2(age='0-20')
-  #   train_aim_2(age='20-40')
+  if args.train_baseline:
+    train_aim_2_baseline(model) 
+    test_aim_2_baseline(model, test_ds) 
     
-  #   test_aim_2(sex='M')
-  #   test_aim_2(sex='F')
-  #   test_aim_2(age='0-20')
-  #   test_aim_2(age='20-40')
-    
-  # if args.train_test_andrew:
-  #   train_aim_2(age='40-60')
-  #   train_aim_2(age='60-80')
-  #   train_aim_2(age='80+')
-  #   train_aim_2(sex='M', age='0-20')
-  #   train_aim_2(sex='M', age='20-40')
-    
-  #   test_aim_2(age='40-60')
-  #   test_aim_2(age='60-80')
-  #   test_aim_2(age='80+')
-  #   test_aim_2(sex='M', age='0-20')
-  #   test_aim_2(sex='M', age='20-40')
-    
-  # if args.train_test_nithya:
-  #   train_aim_2(sex='M', age='40-60')
-  #   train_aim_2(sex='M', age='60-80')
-  #   train_aim_2(sex='M', age='80+')
-  #   train_aim_2(sex='F', age='0-20')
-    
-  #   test_aim_2(sex='M', age='40-60')
-  #   test_aim_2(sex='M', age='60-80')
-  #   test_aim_2(sex='M', age='80+')
-  #   test_aim_2(sex='F', age='0-20')
-    
-  # if args.train_test_skylar:
-  #   train_aim_2(sex='F', age='20-40')
-  #   train_aim_2(sex='F', age='40-60')
-  #   train_aim_2(sex='F', age='60-80')
-  #   train_aim_2(sex='F', age='80+')
-    
-  #   test_aim_2(sex='F', age='20-40')
-  #   test_aim_2(sex='F', age='40-60')
-  #   test_aim_2(sex='F', age='60-80')
-  #   test_aim_2(sex='F', age='80+')
-  
-  # if args.train_test_hulk0:
-  #   train_test_aim_2(sex='M')
-  
-  # if args.train_test_hulk1:
-  #   train_test_aim_2(sex='F')
-    
-  # if args.train_test_hulk2:
-  #   train_aim_2_baseline()
-  #   test_aim_2_baseline()
-    
-  # if args.train_test_andrew:
-  #   train_test_aim_2(age='0-20')
-  #   train_test_aim_2(age='20-40')
-    
-  # if args.train_test_nithya:
-  #   train_test_aim_2(age='40-60')
-  #   train_test_aim_2(age='60-80')
-    
-  # if args.train_test_skylar:
-  #   train_test_aim_2(age='80+')
-  #   train_test_aim_2(sex='M', age='0-20')
-    
-  # if args.train_test_hulk6:
-  #   train_test_aim_2(sex='F', age='60-80') 
-  #   train_test_aim_2(sex='M', age='0-20') 
-    
-  # if args.train_test_hulk7:  
-  #   train_test_aim_2(sex='F', age='80+')
-    
+  #### NOTE: Feel free to parallelize this! 
+  if args.train:
+    print(model, test_ds)
+    # Sex Groups
+    train_test_aim_2(sex='M')
+    train_test_aim_2(sex='F')
+    # Age Groups
+    train_test_aim_2(age='0-20')
+    train_test_aim_2(age='20-40')
+    train_test_aim_2(age='40-60')
+    train_test_aim_2(age='60-80')
+    train_test_aim_2(age='80+')
+    # Intersectional Subgroups - only for DenseNet121
+    if model == 'densenet':
+      train_test_aim_2(sex='M', age='0-20')
+      train_test_aim_2(sex='M', age='20-40')
+      train_test_aim_2(sex='M', age='40-60')
+      train_test_aim_2(sex='M', age='60-80')
+      train_test_aim_2(sex='M', age='80+')
+      train_test_aim_2(sex='F', age='0-20')
+      train_test_aim_2(sex='F', age='20-40')
+      train_test_aim_2(sex='F', age='40-60')
+      train_test_aim_2(sex='F', age='60-80')
+      train_test_aim_2(sex='F', age='80+')  
+      
   if args.test:
     print(model, test_ds)
-    test_aim_2_baseline(model, test_ds)
+    # Sex Groups
     test_aim_2(model, test_ds, sex='M')
     test_aim_2(model, test_ds, sex='F')
+    # Age Groups
     test_aim_2(model, test_ds, age='0-20')
     test_aim_2(model, test_ds, age='20-40')
     test_aim_2(model, test_ds, age='40-60')
     test_aim_2(model, test_ds, age='60-80')
     test_aim_2(model, test_ds, age='80+')
+    # Intersectional Subgroups - only for DenseNet121
     if model == 'densenet':
       test_aim_2(model, test_ds, sex='M', age='0-20')
       test_aim_2(model, test_ds, sex='M', age='20-40')
@@ -145,8 +96,7 @@ if __name__=='__main__':
       test_aim_2(model, test_ds, sex='F', age='20-40')
       test_aim_2(model, test_ds, sex='F', age='40-60')
       test_aim_2(model, test_ds, sex='F', age='60-80')
-      test_aim_2(model, test_ds, sex='F', age='80+')
-      
+      test_aim_2(model, test_ds, sex='F', age='80+')  
     
   if args.analyze:
     # analyze_aim_1_pnm()
